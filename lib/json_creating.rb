84c6a423de482fb651acda8bea4bf6fb7f49eced
ajkrish95 <ajkrish95@gmail.com>
2015-07-23 12:25:17 +0530

    Commented and percentages and uncovered lines parsed

--- lib/json_creating.rb
+++ lib/json_creating.rb
@@ -1,129 +1,207 @@
 require 'json'
 
-#my_hash = {"bob.johnson@example.com"=>{"first"=>"Bob", "last"=>"Johnson"}, "lisa.dell@example.com"=>{"first"=>"Lisa", "last"=>"Dell"}}
-
-#superdirectory = Hash.new() 
-
-
-
-# superdirectory["directory1"] = directory
-
-
 # This function inserts the directories into the hash map
 def insert_directory(directory, directory_name)
-	if !directory.has_key?(directory_name)
-		directory[directory_name] = Hash.new()
-	end
+    if !directory.has_key?(directory_name)
+        directory[directory_name] = Hash.new{}
+    end
 end
 
 # This function inserts the file into the hash of directories
 def insert_file_into_directory(file_name, directory, directory_name)
-	dir_hash = directory[directory_name]
-	dir_hash[file_name] = Hash.new()
+    dir_hash = directory[directory_name]
+    dir_hash[file_name] = Hash.new{}
 end
 
 # This is the main hash - directory level has
-directory = Hash.new()
+directory = Hash.new{}
 
-#This contains the info-file
+# This contains the info-file
 f = open("info_sample")
 
 # regex to check for file name in the info-file
 file_name_regex = /(SF:)(.*)/
 
-#We go through the info-file line by line and get the names of all the directories
-f.each_line {|line| 
-	matched_file = file_name_regex.match(line)
-
-	if matched_file
-		file_name = matched_file[2].dup
-		directory_name = File.dirname(file_name)
-		insert_directory(directory, directory_name)
-	end
+# We go through the info-file line by line and get the names of all the directories
+f.each_line { |line|
+    matched_file = file_name_regex.match(line)
+    
+    if matched_file
+        file_name = matched_file[2].dup
+        directory_name = File.dirname(file_name)
+        insert_directory(directory, directory_name)
+    end
 }
 
 f.close
 
 f = open("info_sample")
 
-#We go through the info-file line by line to get all the names of the different files and put them under the respective directories.
-f.each_line {|line| 
-	matched_file = file_name_regex.match(line)
+# We go through the info-file line by line to get all the names of the
+# different files and put them under the respective directories.
 
-	if matched_file
-		file_name = matched_file[2].dup
-		directory_name = File.dirname(file_name)
-		insert_file_into_directory(file_name, directory, directory_name)
-	end
+f.each_line {|line|
+    matched_file = file_name_regex.match(line)
+    
+    if matched_file
+        file_name = matched_file[2].dup
+        directory_name = File.dirname(file_name)
+        insert_file_into_directory(file_name, directory, directory_name)
+    end
 }
 
 f.close
 
 f = open("info_sample")
 
-#regex to find the line in the info-file which contains the information of where a function starts and its name
+# regex to find the line in the info-file which contains the information
+# of where a function starts and its name
 funtion_name_regex = /(FN:)(.*,)(.*)/
 
 present_file = ""
 present_directory = ""
-present_directory_hash = Hash.new()
+present_directory_hash = Hash.new{}
 present_file_hash = Hash.new()
 prev = -1
 start = Hash.new(0)
 finish = Hash.new(0)
 
-#We go through the info-file line by line to get all the names of the different functions and put them under their respective files.
-f.each_line {|line| 
-	matched_file = file_name_regex.match(line)
-
-	if matched_file
-		present_file = matched_file[2].dup
-		present_directory = File.dirname(present_file)
-		present_directory_hash = directory[present_directory]
-		present_file_hash = present_directory_hash[present_file]
-	end
-
-	m =  funtion_name_regex.match(line)
-
-	if m 
-		function_start_string = m[2].dup.chop
-		function_name = m[3].dup
-		function_start = Integer(function_start_string)
-
-
-		if function_name[0] != '_'
-			function_name.slice!(0)
-			present_file_hash[function_name] = Hash.new()
-
-			start[function_name] = function_start
-
-			if function_start != prev
-				finish[prev] = function_start
-				prev = function_start
-			end
-
-		end
-	end
-
-}
-
-file_json = File.new("out_json.json", "w+")	
-#file_json.puts(directory.to_json)
-file_json.puts(JSON.pretty_generate(directory))
-my_json = directory.to_json
-#puts JSON.pretty_generate(my_json)
-regex4 = /(FNDA:)(.*,)(.*)/
+end_of_file_regex = /(end_of_record)(.*)/
 regex5 = /(DA:)(.*,)(.*)/
-regex1 = /(LH:)(.*)/
-prev = -1
-start = Hash.new(0)
-finish = Hash.new(0)
-count = Hash.new(0)
-hit = Hash.new(0)
+regex4 = /(FNDA:)(.*,)(.*)/
 
+hit = Hash.new (0)
 
+# We go through the info-file line by line to get all the names of the different
+# functions and put them under their respective files. We also find which lines
+# are not covered and also the percentage.
 
-
-
-
-
+f.each_line {|line|
+    matched_file = file_name_regex.match(line)
+    
+    if matched_file
+        present_file = matched_file[2].dup
+        present_directory = File.dirname(present_file)
+        present_directory_hash = directory[present_directory]
+        present_file_hash = present_directory_hash[present_file]
+    end
+    
+    end_of_file = end_of_file_regex.match(line)
+    
+    if end_of_file
+        
+        total_lines = 0 # total number of lines in this file
+        executed_lines = 0 # number of lines executed at least once in this file
+        
+        
+        start.each_pair {|key, value|
+            #			puts key
+            
+            #puts present_file
+            present_function_hash = present_file_hash[key]
+            
+            i = start[key]
+            j = finish[value]
+            
+            
+            ct1 = 0
+            ct2 = 0
+            
+            while i < j do
+                if hit[i] == -1
+                    ct1 += 1
+                    total_lines += 1
+                    rt = IO.readlines(present_file)[i-1]
+                    present_file_hash[key.dup][i] = rt
+                end
+                if hit[i] > 0
+                    ct2 += 1
+                    total_lines += 1
+                    executed_lines += 1
+                end
+                i += 1
+            end
+            
+            #	puts key
+            #	puts "jknkjh"
+            
+            if !present_function_hash
+                puts "gggggg"
+                puts key
+                puts present_file
+            end
+            present_function_hash["__total__lines__"] = (ct1+ct2).to_s
+            present_function_hash["__executed__lines__"] = ct2.to_s
+            
+            }
+            
+            present_file_hash["__total__lines__"] = total_lines.to_s
+            present_file_hash["__executed__lines__"] = executed_lines.to_s
+            
+            start.clear()
+            finish.clear()
+            hit.clear()
+            prev = -1
+            
+            end
+            
+            m =  funtion_name_regex.match(line)
+            
+            if m
+                function_start_string = m[2].dup.chop
+                function_name = m[3].dup
+                function_start = Integer(function_start_string)
+                
+                
+                if function_name[0] != '_'
+                    function_name.slice!(0)
+                    present_file_hash[function_name] = Hash.new{}
+                    
+                    start[function_name] = function_start
+                    
+                    #puts function_name
+                    if function_start != prev
+                        finish[prev] = function_start
+                        prev = function_start
+                    end
+                    
+                end
+            end
+            
+            function_execution = regex4.match(line)
+            
+            if function_execution
+                
+                else
+                matched_line = regex5.match(line)
+                
+                if matched_line
+                    
+                    #		puts matched_line
+                    a = matched_line[2].chop
+                    b = matched_line[3]
+                    
+                    #		puts a
+                    #		puts "fsg"
+                    #		puts b
+                    c = Integer(a)
+                    d = Integer(b)
+                    
+                    hit[c] = d
+                    
+                    if d == 0
+                        hit[c] = -1
+                    end
+                end
+            end
+            
+            
+        }
+        
+        f.close
+        
+        file_json = File.new("out_json.json", "w+")	
+        
+        file_json.puts(JSON.pretty_generate(directory))
+        
+        regex4 = /(FNDA:)(.*,)(.*)/
--- lib/json_parsing.rb
+++ lib/json_parsing.rb
@@ -1,30 +1,35 @@
 require 'json'
 
+# Write the heading into the html file using the
+#  required syntax
+
 def htmlheadingprint(fileHTML, heading)
-	fileHTML.puts("<!DOCTYPE html")
-	fileHTML.puts("<html>")
-	fileHTML.puts("<body>")
-	fileHTML.puts("<h1>")
-	fileHTML.puts(heading)	
-	fileHTML.puts("</h1>")
+    fileHTML.puts("<!DOCTYPE html>")
+    fileHTML.puts("<html>")
+    fileHTML.puts("<body>")
+    fileHTML.puts("<h1>")
+    fileHTML.puts(heading)
+    fileHTML.puts("</h1>")
 end
+
+# Creates a link called string2 which goes to string1.html
+# which is done using href in html
 
 def hrefprint(fileHTML, string1, string2)
-	fileHTML.puts("<a href=\"")
-	fileHTML.puts(string1)
-	fileHTML.puts(".html \">")
-	fileHTML.puts(string2)
-	fileHTML.puts("</a>")
-	fileHTML.puts("<br>")
+    fileHTML.puts("<a href=\"")
+    fileHTML.puts(string1)
+    fileHTML.puts(".html \">")
+    fileHTML.puts(string2)
+    fileHTML.puts("</a>")
+    fileHTML.puts("<br>")
 end
 
+# Prints  string1 as a paragraph into the html file
 def paraprint(fileHTML, string1)
-	fileHTML.puts("<p>")
-	fileHTML.puts(string1)
-	fileHTML.puts("</p>")					
+    fileHTML.puts("<p>")
+    fileHTML.puts(string1)
+    fileHTML.puts("</p>")
 end
-
-#file = File.open("path-to-file.tar.g", "rb")
 
 s = IO.read('out_json.json')
 
@@ -32,57 +37,105 @@ s = IO.read('out_json.json')
 data = JSON.parse(s)
 
 fileHTML = File.new("index.html", "w+")
-htmlheadingprint fileHTML, "Directories"
+htmlheadingprint(fileHTML, "Directories")
 fileHTML.close
 
-fileHTML = File.open("index.html", "a")	
+fileHTML = File.open("index.html", "a")
 
 data.each_pair {|key, value|
-	directory_name = key.dup
-	directory_name_without_symbols = key.dup.delete!'/'
-	hrefprint(fileHTML, directory_name_without_symbols, directory_name)
-	directory_HTML = File.new(directory_name_without_symbols.concat(".html"), "w+")
-	htmlheadingprint directory_HTML, "Files"
-
-	directory_HTML.close
+    directory_name = key.dup
+    directory_name_without_symbols = key.dup.delete!'/'
+    hrefprint(fileHTML, directory_name_without_symbols, directory_name)
+    directory_HTML = File.new(directory_name_without_symbols.concat(".html"), "w+")
+    htmlheadingprint directory_HTML, "Files"
+    
+    directory_HTML.close
 }
 
 fileHTML.close
 
 data.each_pair {|key, value|
-	directory_name= key.dup.delete!'/'
-
-	directory_name.concat(".html")
-
-	directoryHTML = File.open(directory_name, "a")
-
-	value.each_pair {|key1, value1|
-		file_name = key1.dup
-		file_name_without_symbols = key1.dup.delete!'/'
-		hrefprint(directoryHTML, file_name_without_symbols, file_name)
-		file_HTML = File.new(file_name_without_symbols.concat(".html"), "w+")
-		htmlheadingprint file_HTML, "Functions..."
-
-		file_HTML.close
-	}
-
-	directoryHTML.close
+    directory_name= key.dup.delete!'/'
+    
+    directory_name.concat(".html")
+    
+    directoryHTML = File.open(directory_name, "a")
+    
+    value.each_pair {|key1, value1|
+        file_name = key1.dup
+        file_name_without_symbols = key1.dup.delete!'/'
+        hrefprint(directoryHTML, file_name_without_symbols, file_name)
+        file_HTML = File.new(file_name_without_symbols.concat(".html"), "w+")
+        htmlheadingprint file_HTML, "Functions...."
+        
+        file_HTML.close
+    }
+    
+    directoryHTML.close
 }
 
 data.each_pair {|key,value|
-	value.each_pair {|key1, value1|
-		file_name_without_symbols = key1.dup.delete!'/'
-		file_HTML = File.open(file_name_without_symbols.concat(".html"), "a")
-
-		value1.each_pair {|key2, value2|
-			function_name = key2.dup
-			augmented_function_name =  file_name_without_symbols.concat(function_name)
-			hrefprint(file_HTML, augmented_function_name.concat(".html"), function_name)		
-		}
-	}
+    value.each_pair {|key1, value1|
+        file_name_without_symbols = key1.dup.delete!'/'
+        file_name_without_symbols_html = file_name_without_symbols.dup.concat(".html")
+        file_HTML = File.open(file_name_without_symbols_html, "a")
+        
+        value1.each_pair {|key2, value2|
+            
+            if key2 == "__total__lines__" || key2 == "__total__lines__"
+                next
+            end
+            
+            function_name = key2.dup
+            augmented_function_name =  file_name_without_symbols.dup.concat(function_name)
+            
+            #	puts augmented_function_name
+            #	puts " "
+            total_lines = Float(value2["__total__lines__"])
+            executed_lines = Float(value2["__executed__lines__"])
+            
+            if total_lines < 1.0
+                total_lines = 1.0
+            end
+            percentage = executed_lines/total_lines
+            percentage = percentage*100.0
+            percentage = Integer(percentage)
+            percent = percentage.to_s
+            #			puts percentage
+            fun_name = ("&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; coverage = ").concat(percent).concat(" %")
+            
+            #puts fun_name
+            hrefprint(file_HTML, augmented_function_name, function_name)
+            #	puts augmented_function_name
+            paraprint(file_HTML, fun_name)
+            function_HTML = File.new(augmented_function_name.dup.concat(".html"), "w+")
+            htmlheadingprint function_HTML, "Uncovered Lines......"
+            function_HTML.close
+        }
+    }
 }
 
+data.each_pair {|key,value|
+    value.each_pair {|key1, value1|
+        file_name_without_symbols = key1.dup.delete!'/'
+        
+        value1.each_pair {|key2, value2|
+            function_name = key2.dup
+            augmented_function_name =  file_name_without_symbols.dup.concat(function_name)
+            
+            function_HTML = File.open(augmented_function_name.dup.concat(".html"), "a")
+            
+            value2.each_pair {|key3,value3|
+                
+                if(key3 != "__total__lines__" && key3 != "__executed__lines__")
+                    uncovered_line = key3.dup.concat(")    ").dup.concat(value3.dup)
+                    paraprint(function_HTML, uncovered_line)
+                end
+            }
+        }
+    }
+}
 
 system(" open index.html")
 
-	
+
